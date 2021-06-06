1b6463adc800445bccf04508ea53b48b229363e9
CharlieDavid2021 <769299674q@gmail.com>
2021-06-06 21:12:05 +0800

    Initial Commit

---
+++ Shared/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Shared
+//
+//  Created by CharlieDavid on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
